f5e91987b1721208438fc3ae949cd4b311425444
philippaul1998 <pippu98@gmail.com>
2019-03-11 22:34:04 +0530

    white

--- app/src/main/java/com/eve/dyuthi/ScrollingActivity.java
+++ app/src/main/java/com/eve/dyuthi/ScrollingActivity.java
@@ -15,8 +15,9 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.dark));
 
     }
 

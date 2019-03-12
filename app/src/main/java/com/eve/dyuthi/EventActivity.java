d278684454d66e70b2cf52376ebdf5f62d4be2b8
philippaul1998 <pippu98@gmail.com>
2019-03-12 16:45:22 +0530

    Event details

--- app/src/main/java/com/eve/dyuthi/EventActivity.java
+++ app/src/main/java/com/eve/dyuthi/EventActivity.java
@@ -1,20 +1,40 @@
 package com.eve.dyuthi;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 public class EventActivity extends AppCompatActivity {
 
+
+    ImageView toolbar_image;
+    CollapsingToolbarLayout ctb;
+    int image_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ctb = findViewById(R.id.collapsingToolbar);
+        toolbar_image = findViewById(R.id.toolbarImage);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -26,9 +46,75 @@ public class EventActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //setColor();
+        setImagePro();
+
+    }
+
+    private void setImagePro() {
+        image_id = getIntent().getIntExtra("image_id",0);
+        if(image_id!=0){
+            Glide.with(this).load(image_id).into(toolbar_image);
+            getSupportActionBar().setTitle(getIntent().getStringExtra("name"));
+        }
+
+    }
+
+    public void setColor(){
+        Glide.with(this).asBitmap().
+                load(R.drawable.agam).
+                into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        toolbar_image.setImageBitmap(resource);
+                        toolbar_image.setScaleType(ImageView.ScaleType.CENTER);
+                        Palette.from(resource)
+                                .generate(new Palette.PaletteAsyncListener() {
+                                    @Override
+                                    public void onGenerated(@Nullable Palette palette) {
+                                        Palette.Swatch textSwatch = palette.getVibrantSwatch();
+                                        GradientDrawable gd;
+                                        if (textSwatch == null) {
+                                            gd = new GradientDrawable(
+                                                    GradientDrawable.Orientation.BOTTOM_TOP,
+                                                    new int[]{0xff000000, 0x00000000});
+                                        }else {
+                                            gd = new GradientDrawable(
+                                                    GradientDrawable.Orientation.BOTTOM_TOP,
+                                                    new int[]{textSwatch.getRgb(), 0x00000000}
+
+                                            );
+                                        }
+                                        toolbar_image.setBackground(gd);
+                                        Window window = getWindow();
+//
+                                        // clear FLAG_TRANSLUCENT_STATUS flag:
+                                        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+                                        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+                                        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+                                        // finally change the color
+                                        if (textSwatch!= null) {
+                                            window.setStatusBarColor(textSwatch.getRgb());
+                                            ctb.setStatusBarScrimColor(textSwatch.getRgb());
+                                            ctb.setContentScrimColor(textSwatch.getRgb());
+                                        }else{
+                                            int black = Color.rgb(0,0,0);
+                                            window.setStatusBarColor(black);
+                                            ctb.setStatusBarScrimColor(black);
+                                            ctb.setContentScrimColor(black);
+                                        }
+
+                                    }
+                                });
+                    }
+                });
     }
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
+        super.onBackPressed();
         ActivityCompat.finishAfterTransition(this);
     }
 }
--- app/src/main/java/com/eve/dyuthi/ScrollingActivity.java
+++ app/src/main/java/com/eve/dyuthi/ScrollingActivity.java
@@ -1,13 +1,16 @@
 package com.eve.dyuthi;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -17,7 +20,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class ScrollingActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -37,6 +43,10 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
      */
     private ViewPager mViewPager;
     CardView day2,day3,day4;
+    ImageView dayImage2,dayImage3,dayImage4;
+    String url1 = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi4p-rytfzgAhVWOSsKHaTIBDcQjRx6BAgBEAU&url=https%3A%2F%2Fwww.justdial.com%2Fentertainment%2Fartist%2FGowry-Lekshmi%2FA839744&psig=AOvVaw3BRchgYeyVzfCZBefmzMly&ust=1552473809853704";
+    String url2 = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiIwdbTtfzgAhWPfX0KHWHPDnEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.oklisten.com%2Falbum%2Fsaagara_shayana_vibho&psig=AOvVaw0PGXOhvjev17FTAVyX6_az&ust=1552473763489338";
+    String url3 = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjBwNWDtvzgAhXEe30KHYnGBaAQjRx6BAgBEAU&url=https%3A%2F%2Freacho.in%2Fpune%2Fevent%2Fkingfisher-buzz-presents-nucleya-raja-baja-album-launch&psig=AOvVaw02Se9ysrhha9_eyLaVsxPn&ust=1552473869064267";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +57,9 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
         day2 = findViewById(R.id.day2);
         day3 = findViewById(R.id.day3);
         day4 = findViewById(R.id.day4);
+        dayImage2 = findViewById(R.id.dayImg2);
+        dayImage3 = findViewById(R.id.dayImg3);
+        dayImage4 = findViewById(R.id.dayImg4);
         day2.setOnClickListener(this);
         day3.setOnClickListener(this);
         day4.setOnClickListener(this);
@@ -55,7 +68,9 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
         mViewPager = findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-
+        Glide.with(this).load(R.drawable.day2).into(dayImage2);
+        Glide.with(this).load(R.drawable.agam).into(dayImage3);
+        Glide.with(this).load(R.drawable.nucleya).into(dayImage4);
         TabLayout tabLayout = findViewById(R.id.tabs);
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
@@ -68,9 +83,39 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.day2:
+                Intent intent = new Intent(ScrollingActivity.this  , EventActivity.class);
+                intent.putExtra("image_id",R.drawable.day2);
+                intent.putExtra("name","Live Concert");
+                ActivityOptionsCompat options =ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        this,
+                         dayImage2,
+                        "simple_activity_transition");
+
+                startActivity(intent , options.toBundle());
+                break;
             case R.id.day3:
+                Intent intent2 = new Intent(ScrollingActivity.this  , EventActivity.class);
+                intent2.putExtra("image_id",R.drawable.agam);
+                intent2.putExtra("name","Agam");
+                ActivityOptionsCompat options2 =ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        this,
+                        dayImage3,
+                        "simple_activity_transition");
+
+                startActivity(intent2 , options2.toBundle());
+                break;
+
             case R.id.day4:
-                startActivity(new Intent(getApplicationContext(),EventActivity.class));break;
+                Intent intent3 = new Intent(ScrollingActivity.this  , EventActivity.class);
+                intent3.putExtra("image_id",R.drawable.nucleya);
+                intent3.putExtra("name","Nucleya");
+                ActivityOptionsCompat options3 =ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        this,
+                        dayImage4,
+                        "simple_activity_transition");
+
+                startActivity(intent3 , options3.toBundle());
+                break;
         }
     }
 
